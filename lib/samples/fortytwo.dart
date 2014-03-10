6278d70cadea8b7c541075c2ad68d977ad111d9e
Greg Tener <borisb0ri5@gmail.com>
2014-03-10 09:08:44 -0700

    Add a few more samples of language features

--- lib/samples/fortytwo.dart
+++ lib/samples/fortytwo.dart
@@ -1,3 +1,34 @@
 library fortytwo;
 
-int fortytwo() => 42;
+int fortytwoArrowFunction() => 42;
+
+int fortytwoNamedFunction() {
+  return 42;
+}
+
+untypedReturnsFortyTwoFunction() {
+  return () => 42;
+}
+
+Function typedReturnsFortyTwoFunction() {
+  return () {
+    return 42;
+  };
+}
+
+typedef int IntFunction();
+
+IntFunction typedefReturnsFortyTwoFunction() {
+  return () {
+    return 42;
+  };
+}
+
+class FortyTwo {
+  final int value;
+  const FortyTwo({this.value : 42});
+  FortyTwo.named(): this(42);
+
+  int fortytwo() => 42;
+  int get sixtimesseven => 6 * 7;
+}
--- test/samples/fortytwo_test.dart
+++ test/samples/fortytwo_test.dart
@@ -3,6 +3,13 @@ import 'package:solvents-dart/samples/fortytwo.dart';
 
 void main() {
   test('fortytwo', () {
-    expect(fortytwo(), equals(42));
+    expect(fortytwoArrowFunction(), equals(42));
+    expect(fortytwoNamedFunction(), equals(42));
+    expect(untypedReturnsFortyTwoFunction()(), equals(42));
+    expect(typedReturnsFortyTwoFunction()(), equals(42));
+    expect(typedefReturnsFortyTwoFunction()(), equals(42));
+    expect(new FortyTwo().sixtimesseven, equals(42));
+    expect(new FortyTwo().fortytwo(), equals(42));
+    expect(new FortyTwo().value, equals(42));
   });
 }
